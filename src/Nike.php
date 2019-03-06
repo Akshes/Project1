c1cb6bafabeb4118a6571fbe7ac1fbed2fe8ff0f
Akshesh Patel <47195718+Akshes@users.noreply.github.com>
2019-03-06 12:19:48 -0500

    Wrote a code inside nike.php to to see what type of shoe it is.

--- src/Nike.php
+++ src/Nike.php
@@ -5,12 +5,27 @@ class nike extends shoes
     public function MenOrWomenShoe(String $input): String
     {
         if (strpos($input, "Men"))
-            (
+        {
+
                 echo("This Shoe is for Men");
-            )
+        }
             else if (strpos($input, "Women")) {
                 echo("This Shoe is for Women");
 
+            }
+    }
+}
+
+{
+    public function TypeOfShoe(String $input): String
+    {
+        if (strpos($input, "Running")) {
+
+            echo("This is a running shoe");
+        }
+            else if (strpos($input, "Casual")) {
+                echo("This is a casual shoe");
         }
     }
 }
+
